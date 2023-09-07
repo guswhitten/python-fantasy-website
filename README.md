4522ca8c64c7000213e4310be94c4bc3ac850218
whit o <whittenoswald@gmail.com>
2023-09-07 08:17:27 -0400

    get project working

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-To run the Flask app:
+## Dependencies:
 
-In VS Code, open myproject.py and press F5, or click the green play button in the top right corner of the editor. This will run the Flask development server on your local machine, and you'll be able to visit the website at http://localhost:8000
+- `pip install flask`
+- `pip install bar_chart_race`
+- `pip install espn_api`
+- `choco install ffmpeg` or download here https://ffmpeg.org/download.html
+
+## How to use:
+
+Open myproject.py and press F5, or click the green play button in the top right corner of the VS Code editor. This will run the Flask development server on your local machine, and you'll be able to visit the website at http://localhost:8000
--- views.py
+++ views.py
@@ -4,24 +4,7 @@ from get_league_data import create_chart_race
 
 views = Blueprint(__name__, "views")
 
-
-@views.route("/home")
-@views.route("/")
-def home_page():
-    return render_template("index.html")
-
-
-@views.route("/projects")
-def projects():
-    return render_template("projects.html")
-
-
-@views.route("/arcade")
-def arcade():
-    return render_template("arcade.html")
-
-
-@views.route("/fantasy", methods=["GET", "POST"])
+@views.route("/", methods=["GET", "POST"])
 def fantasy():
     display = False
     if request.method == "POST":
@@ -30,6 +13,6 @@ def fantasy():
         league = football.League(league_id=int(league_id), year=int(year))
         create_chart_race(league)
         display = True
-        return render_template("fantasy.html", display=display, len=len(league.teams))
+        return render_template("index.html", display=display, len=len(league.teams))
     else:
-        return render_template("fantasy.html")
+        return render_template("index.html")
